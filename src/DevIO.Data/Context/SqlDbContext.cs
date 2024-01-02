3689ae2f6a19498fefe5ce8dd4149a21a45a67ba
Jean Pablo Carvalho da Silva <jpablo1026@hotmail.com>
2024-01-02 15:37:29 -0300

    instalando libs do ef core e add referencias

---
+++ src/DevIO.Data/Context/SqlDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Context
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+    }
+}
