ee16e3d2f6173d29ba7e744cb8ad8f834c43681d
Jean Pablo Carvalho da Silva <jpablo1026@hotmail.com>
2024-01-09 10:55:02 -0300

    implementacao controller

---
+++ src/DevIO.API/Controllers/MainController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.API.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+
+    }
+}
---
+++ src/DevIO.API/Controllers/ProdutosController.cs
@@ -0,0 +1,37 @@
+﻿using DevIO.API.DTOs;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+
+namespace DevIO.API.Controllers
+{
+    [Route("api/produtos")]
+    public class ProdutosController
+    {
+        [HttpGet]
+        public async Task<ActionResult<ProdutoDTO>> ObterTodos()
+        {
+
+        }
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<ProdutoDTO>> ObterPorId(Guid id)
+        {
+
+        }
+        [HttpPost]
+        public async Task<ActionResult<ProdutoDTO>> Adicionar(ProdutoDTO produto)
+        {
+
+        }
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<ProdutoDTO>> Atualizar(Guid id, ProdutoDTO produto)
+        {
+
+        }
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<ProdutoDTO>> Excluir(Guid id)
+        {
+
+        }
+    }
+}
